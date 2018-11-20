7676bf3298ebd14614d5a8d8153ea12d8bce6b45
Martin D'Souza <martin@clarifit.com>
2018-11-19 23:19:58 -0700

    fixed bug with call to apex_rest_config_core.sql"

--- files/apex-install.sql
+++ files/apex-install.sql
@@ -29,7 +29,8 @@ end;
 @apexins.sql SYSAUX SYSAUX TEMP /i/
 
 -- APEX REST configuration
-@apex_rest_config_core.sql oracle oracle
+-- In APEX 18.2 they're 3 parameters for this file
+@apex_rest_config_core.sql @ oracle oracle
 
 -- Required for ORDS install
 alter user apex_public_user identified by oracle account unlock;
